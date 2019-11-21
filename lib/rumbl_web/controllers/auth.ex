d624589d67c859f911ff9684cbbbcb3c14c08625
marth141 <ncasados@live.com>
2019-11-21 16:16:45 -0700

    Fixing spelling of name for Login implementation

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,4 +1,4 @@
-defmodule RumbleWeb.Auth do
+defmodule RumblWeb.Auth do
   import Plug.Conn
 
   def init(opts), do: opts
@@ -7,5 +7,12 @@ defmodule RumbleWeb.Auth do
     user_id = get_session(conn, :user_id)
     user = user_id && Rumbl.Accounts.get_user(user_id)
     assign(conn, :current_user, user)
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
   end
 end
