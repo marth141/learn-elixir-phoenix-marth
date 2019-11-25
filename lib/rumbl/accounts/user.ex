f87d6b7619af7ac6eb05d60068cdec081bcf8321
marth141 <ncasados@live.com>
2019-11-25 15:45:32 -0700

    Start In-context Relationships Chap 6

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -2,6 +2,15 @@ defmodule Rumbl.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
   def registration_changeset(user, params) do
     user
     |> changeset(params)
@@ -25,14 +34,5 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
-  end
-
-  schema "users" do
-    field :name, :string
-    field :username, :string
-    field :password, :string, virtual: true
-    field :password_hash, :string
-
-    timestamps()
   end
 end
