f2c73cbc1ba2512be6983a2ab61c2ef84b21a970
marth141 <ncasados@live.com>
2019-11-26 16:25:11 -0700

    Start Chap 9 Removed unnecessary Test

--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -2,6 +2,26 @@ defmodule RumblWeb.VideoControllerTest do
   use RumblWeb.ConnCase, async: true
 
   describe "with a logged-in user" do
+    setup %{conn: conn, login_as: username} do
+      user = user_fixture(username: username)
+      conn = assign(conn, :current_user, user)
+
+      {:ok, conn: conn, user: user}
+    end
+
+    @tag login_as: "max"
+    test "lists all user's videos on index", %{conn: conn, user: user} do
+      user_video = video_fixture(user, title: "funny cats")
+      other_video = video_fixture(
+        user_fixture(username: "other"),
+        title: "another video")
+      conn = get conn, Routes.video_path(conn, :index)
+      response = html_response(conn, 200)
+      assert response =~ ~r/Listing Videos/
+      assert response =~ user_video.title
+      refute response =~ other_video.title
+    end
+
     alias Rumbl.Multimedia
 
     @create_attrs %{
@@ -32,63 +52,28 @@ defmodule RumblWeb.VideoControllerTest do
       assert html_response(conn, 200) =~ "check the errors"
       assert video_count() == count_before
     end
-
-    test "authorizes actions against access by other users", %{conn: conn} do
-      owner = user_fixture(username: "owner")
-      video = video_fixture(owner, @create_attrs)
-      non_owner = user_fixture(username: "sneaky")
-      conn = assign(conn, :current_user, non_owner)
-
-      assert_error_sent :not_found, fn ->
-        get(conn, Routes.video_path(conn, :show, video))
-      end
-
-      assert_error_sent :not_found, fn ->
-        get(conn, Routes.video_path(conn, :edit, video))
-      end
-
-      assert_error_sent :not_found, fn ->
-        get(conn, Routes.video_path(conn, :update, video))
-      end
-
-      assert_error_sent :not_found, fn ->
-        get(conn, Routes.video_path(conn, :delete, video))
-      end
-    end
-
-    setup %{conn: conn, login_as: username} do
-      user = user_fixture(username: username)
-      conn = assign(conn, :current_user, user)
-
-      {:ok, conn: conn, user: user}
-    end
-
-    @tag login_as: "max"
-    test "lists all user's videos on index", %{conn: conn, user: user} do
-      user_video = video_fixture(user, title: "funny cats")
-      other_video = video_fixture(
-        user_fixture(username: "other"),
-        title: "another video")
-      conn = get conn, Routes.video_path(conn, :index)
-      response = html_response(conn, 200)
-      assert response =~ ~r/Listing Videos/
-      assert response =~ user_video.title
-      refute response =~ other_video.title
-    end
   end
 
-  test "requires user authentication on all actions", %{conn: conn} do
-    Enum.each([
-      get(conn, Routes.video_path(conn, :new)),
-      get(conn, Routes.video_path(conn, :index)),
-      get(conn, Routes.video_path(conn, :show, "123")),
-      get(conn, Routes.video_path(conn, :edit, "123")),
-      get(conn, Routes.video_path(conn, :update, "123", %{})),
-      get(conn, Routes.video_path(conn, :create, %{})),
-      get(conn, Routes.video_path(conn, :delete, "123")),
-    ], fn conn ->
-      assert html_response(conn, 302)
-      assert conn.halted
-    end)
+  test "authorizes actions against access by other users", %{conn: conn} do
+    owner = user_fixture(username: "owner")
+    video = video_fixture(owner, @create_attrs)
+    non_owner = user_fixture(username: "sneaky")
+    conn = assign(conn, :current_user, non_owner)
+
+    assert_error_sent :not_found, fn ->
+      get(conn, Routes.video_path(conn, :show, video))
+    end
+
+    assert_error_sent :not_found, fn ->
+      get(conn, Routes.video_path(conn, :edit, video))
+    end
+
+    assert_error_sent :not_found, fn ->
+      get(conn, Routes.video_path(conn, :update, video, video: @create_attrs))
+    end
+
+    assert_error_sent :not_found, fn ->
+      get(conn, Routes.video_path(conn, :delete, video))
+    end
   end
 end
